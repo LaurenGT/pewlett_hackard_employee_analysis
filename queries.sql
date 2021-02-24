2860aa2dce0a62f69c11aeef83428422743c7140
LaurenGT <ltoothaker2@gmail.com>
2021-02-24 15:26:57 -0800

    department manager info

--- queries.sql
+++ queries.sql
@@ -29,6 +29,19 @@ WHERE
 
 -- 3. List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
+-- dept_no, dept_name from departments
+-- emp_no from dept_manager on dept_no
+-- last_name, first_name from empoloyees on emp_no
+
+SELECT departments.dept_no,
+    departments.dept_name,
+    dept_manager.emp_no,
+	employees.last_name,
+    employees.first_name
+FROM departments
+INNER JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+INNER JOIN employees ON dept_manager.emp_no = employees.emp_no
+;
 
 
 -- 4. List the department of each employee with the following information: 
