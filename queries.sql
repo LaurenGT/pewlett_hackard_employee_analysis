13f04b6b51d41d897884924d4bd5dd238fad4d5b
LaurenGT <ltoothaker2@gmail.com>
2021-02-24 16:25:59 -0800

    descending last name count

--- queries.sql
+++ queries.sql
@@ -106,3 +106,9 @@ WHERE dept_emp.dept_no = 'd007'
 
 -- 8. In descending order, list the frequency count of employee last names, 
 -- i.e., how many employees share each last name.
+
+SELECT last_name, COUNT (last_name) AS count
+FROM employees
+GROUP BY last_name
+ORDER BY count desc
+;
