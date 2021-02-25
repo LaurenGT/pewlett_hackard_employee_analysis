bcf6841cbf3ef88a91fcafc30d4c8ba176a6f947
LaurenGT <ltoothaker2@gmail.com>
2021-02-24 16:07:10 -0800

    hercules B% employess

--- queries.sql
+++ queries.sql
@@ -26,7 +26,6 @@ WHERE
 	AND hire_date <= '1986-12-31'
 ;
 
-
 -- 3. List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
 -- dept_no, dept_name from departments
@@ -46,9 +45,30 @@ INNER JOIN employees ON dept_manager.emp_no = employees.emp_no
 
 -- 4. List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
+-- emp_no, last_name, first_name from employees
+-- dept_name from departments on dept_no from dept_emp
+-- drop dept_no at end
 
+SELECT employees.emp_no, 
+    employees.last_name, 
+    employees.first_name,
+    departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name,
+    last_name,
+    sex
+FROM employees
+WHERE
+    first_name = 'Hercules'
+	and
+    last_name LIKE 'B%'
+;
 
 
 -- 6. List all employees in the Sales department, 
